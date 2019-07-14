09a8d27468d609af1eb9e660e0d9e0b1c6644546
Albert Montolio <albert.montolio@gmail.com>
2019-07-14 10:43:03 +0200

    swipe delete

--- components/molecules/ResultLog.js
+++ components/molecules/ResultLog.js
@@ -3,6 +3,8 @@ import {
   Text
 } from 'react-native'
 import styled from 'styled-components'
+import Swipeout from 'react-native-swipeout'
+
 import { 
   millisToMinutesAndSeconds,
   formatDate
@@ -22,13 +24,29 @@ const StyledDate = styled.Text`
   width: 200px;
 `
 
+const deleteResult = () => {
+  console.log('deleting result')
+}
+
+var swipeoutBtns = [
+  {
+    text: 'Delete',
+    backgroundColor: 'red',
+    onPress: () => deleteResult()
+  }
+]
+
 const ResultLog = ({train}) => (
-  <StyledResult key={train.start}>
-    <StyledDate>
-      {formatDate(train.start, 'D MMM h:mm a')}
-    </StyledDate>
-    <Text>{millisToMinutesAndSeconds(train.time)}</Text>
-  </StyledResult>
+  <Swipeout 
+    right={swipeoutBtns}
+  >
+    <StyledResult key={train.start}>
+      <StyledDate>
+        {formatDate(train.start, 'D MMM h:mm a')}
+      </StyledDate>
+      <Text>{millisToMinutesAndSeconds(train.time)}</Text>
+    </StyledResult>
+  </Swipeout>
 )
 
 export default ResultLog
